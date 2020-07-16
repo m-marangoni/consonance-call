48e4436dcc26522d11d455efb2f4f15de3928955
Raul Alves <raulbalves@gmail.com>
2020-07-16 16:17:30 +0100

    first test

---
+++ convert.js
@@ -0,0 +1,19 @@
+// const ffmpeg = require("ffmpeg");
+
+// try{
+//     var process = new ffmpeg("/messages")
+//     process.then( function (audio){
+//         //callback mode
+//         audio.fnExtractSoundToMP3("/messsages/new/file.mp3"),  function(error, file){
+//             if (!error) {
+//             console.log('audio file: '+ file);
+//          } };
+//     }, function (err) {
+//         console.log('error: ' + err);
+//     });
+// }
+// catch (e) {
+//     console.log(e.code);
+//     console.log(e.msg);
+
+// }
